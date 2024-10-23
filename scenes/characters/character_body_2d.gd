f2b2e9ee619d8faf13ccb933a55ccb64ad8dd318
filipefs <11069278+filipefs@users.noreply.github.com>
2024-10-23 00:52:13 -0300

    Created the tilemap for interior 1 (bedroom), updated character movement and added basic collision

--- scenes/characters/character_body_2d.gd
+++ scenes/characters/character_body_2d.gd
@@ -1,10 +1,11 @@
-extends Node2D
-var velocity := Vector2(0, 0)
-var max_speed := 150.0
+extends CharacterBody2D
+
+var max_speed := 70.0
 var steering_factor := 10.0
 
+func _physics_process(delta: float) -> void:
+	move_and_slide()
 
-func _process(delta: float) -> void:
 	var direction := Vector2(0,0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
@@ -13,11 +14,9 @@ func _process(delta: float) -> void:
 	if direction.length() > 1.0:
 		direction = direction.normalized()
 	
-	#steering
+	#adiciona momentum ao movimento do personagem
 	var desired_velocity := max_speed * direction
 	var steering_vector := desired_velocity - velocity
-	
 	velocity += steering_vector * steering_factor * delta
+	
 	position += velocity * delta
-	
-	
